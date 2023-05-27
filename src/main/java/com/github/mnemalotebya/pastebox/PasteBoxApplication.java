3da0892f4d6ff08855b0e14eb3514a7782880d4a
Alexander <LipatovAlexander93@yandex.ru>
2023-05-27 16:38:52 +0300

    initial

---
+++ src/main/java/com/github/mnemalotebya/pastebox/PasteBoxApplication.java
@@ -0,0 +1,13 @@
+package com.github.mnemalotebya.pastebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteBoxApplication.class, args);
+	}
+
+}
